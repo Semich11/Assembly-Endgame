4820be1020e26df8ea9b86862b37068ad3e4ad57
semich11 <chima11christopher@gmail.com>
2025-07-03 14:31:13 +0100

    Dice state has a array of object instead of numbers.

--- Documents/ScrimbaCapstoneProject/Tenzies/src/App.jsx
+++ Documents/ScrimbaCapstoneProject/Tenzies/src/App.jsx
@@ -1,33 +1,54 @@
-import { useState } from "react"
-import Die from "./Die"
+import { useState } from "react";
+import Die from "./Die";
 
 export default function App() {
-    
-    const [dice, setDice] = useState([
-      {value: 0, isHeld: false},
-      {value: 1, isHeld: false}
-    ])``
-    
-    function generateAllNewDice() {
-        return new Array(10)
-            .fill(0)
-            .map(() => Math.ceil(Math.random() * 6))
-    }
-    
-    function rollDice() {
-        setDice(generateAllNewDice())
-    }
-    
-    const diceElements = dice.map(num => <Die value={num} />)
-    console.log("Run")
-    return (
-        <main>
-            <div className="dice-container">
-                {diceElements}
-            </div>
-            
-            <button className="roll-dice" onClick={rollDice}>Roll</button>
-            
-        </main>
-    )
+  const [dice, setDice] = useState([
+    { value: 0, isHeld: false },
+    { value: 0, isHeld: false },
+    { value: 0, isHeld: false },
+    { value: 0, isHeld: false },
+    { value: 0, isHeld: false },
+    { value: 0, isHeld: false },
+    { value: 0, isHeld: false },
+    { value: 0, isHeld: false },
+    { value: 0, isHeld: false },
+    { value: 0, isHeld: false },
+  ]);
+
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+  }
+
+  function diceObject() {
+    const randomNumbers = generateAllNewDice();
+
+    setDice((prev) =>
+      prev.map((item, index) =>
+        item.isHeld === false
+          ? {
+              ...item,
+              value: randomNumbers[index],
+            }
+          : item
+      )
+    );
+  }
+
+  console.log(dice[0].value)
+
+  function rollDice() {
+    diceObject();
+  }
+
+  const diceElements = dice.map((num) => <Die value={num} />);
+  console.log("Run");
+  return (
+    <main>
+      <div className="dice-container">{diceElements}</div>
+
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
+    </main>
+  );
 }
