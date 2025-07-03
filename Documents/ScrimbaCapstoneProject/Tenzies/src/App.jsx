d3099bfbbfccbf9556c04ca4ecb061d5e39dbac7
semich11 <chima11christopher@gmail.com>
2025-07-03 13:23:34 +0100

    Implemented roll button. Now we can generate new set of random numbers by clicking on the roll bitton

--- Documents/ScrimbaCapstoneProject/Tenzies/src/App.jsx
+++ Documents/ScrimbaCapstoneProject/Tenzies/src/App.jsx
@@ -1,42 +1,33 @@
+import { useState } from "react"
 import Die from "./Die"
 
-/**
- * Challenge:
- * 
- * Write a function (generateAllNewDice) that returns an array 
- * of 10 random numbers between 1-6 inclusive.
- * 
- * Log the array of numbers to the console for now
- */
-  
-  let arr = []
-
-  const generateAllNewDice = () => {
-    for(let i = 1; i <= 10; i++){
-      let randomNumber = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
-      arr.push(randomNumber);
-    }
-    return arr;
-  }
-  
-  generateAllNewDice()
-  console.log(arr)
-
 export default function App() {
+    
+    const [dice, setDice] = useState([
+      {value: 0, isHeld: false},
+      {value: 1, isHeld: false}
+    ])``
+    
+    function generateAllNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6))
+    }
+    
+    function rollDice() {
+        setDice(generateAllNewDice())
+    }
+    
+    const diceElements = dice.map(num => <Die value={num} />)
+    console.log("Run")
     return (
         <main>
             <div className="dice-container">
-                <Die value={1} />
-                <Die value={2} />
-                <Die value={3} />
-                <Die value={4} />
-                <Die value={5} />
-                <Die value={6} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
+                {diceElements}
             </div>
+            
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            
         </main>
     )
 }
