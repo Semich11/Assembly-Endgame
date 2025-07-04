c3db08f76df490e5797c121afc17fa4c18aa592b
semich11 <chima11christopher@gmail.com>
2025-07-04 16:16:25 +0100

    If all the disce is held and the value od all the dice is the same change gameWon variable to true. and if the gameWon is true button text should be 'New Game' instead of 'Roll'

--- Documents/ScrimbaCapstoneProject/Tenzies/src/App.jsx
+++ Documents/ScrimbaCapstoneProject/Tenzies/src/App.jsx
@@ -5,6 +5,15 @@ import { nanoid } from "nanoid";
 export default function App() {
   const [dice, setDice] = useState(generateAllNewDice());
 
+  let gameWon = false;
+
+  if(
+    dice.every(diceObj => diceObj.isHeld) && 
+    dice.every(diceObj => diceObj.value === dice[0].value )
+  ){
+    gameWon = true;
+  }
+
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => ({
       value: Math.ceil(Math.random() * 6),
@@ -19,7 +28,6 @@ export default function App() {
   }
 
   function hold(id) {
-    console.log(id)
     setDice(prev => prev.map(diceObj => diceObj.id === id ? {...diceObj, isHeld: !diceObj.isHeld} : diceObj))
   }
 
@@ -32,12 +40,13 @@ export default function App() {
   ));
   // console.log("Run");
   return (
-    <main>
-      <div className="dice-container">{diceElements}</div>
-
-      <button className="roll-dice" onClick={rollDice}>
-        Roll
-      </button>
-    </main>
-  );
+        <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button className="roll-dice" onClick={rollDice}>{gameWon? "New Game" : "Roll"}</button>
+        </main>
+    )
 }
