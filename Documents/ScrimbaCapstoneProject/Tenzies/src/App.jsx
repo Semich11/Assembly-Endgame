927e6e5c2d6ebedc7af9d80e7e33bdcebb180235
semich11 <chima11christopher@gmail.com>
2025-07-04 17:00:38 +0100

    Added celebration confetti, and started working on the accessibility.

--- Documents/ScrimbaCapstoneProject/Tenzies/src/App.jsx
+++ Documents/ScrimbaCapstoneProject/Tenzies/src/App.jsx
@@ -1,20 +1,17 @@
 import { useState } from "react";
 import Die from "./Die";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 export default function App() {
-  const [dice, setDice] = useState(generateAllNewDice());
+  const [dice, setDice] = useState(() => generateAllNewDice());
 
-  let gameWon = false;
-
-  if(
-    dice.every(diceObj => diceObj.isHeld) && 
-    dice.every(diceObj => diceObj.value === dice[0].value )
-  ){
-    gameWon = true;
-  }
+  let gameWon =
+    dice.every((diceObj) => diceObj.isHeld) &&
+    dice.every((diceObj) => diceObj.value === dice[0].value);
 
   function generateAllNewDice() {
+    console.log("called!!");
     return new Array(10).fill(0).map(() => ({
       value: Math.ceil(Math.random() * 6),
       isHeld: false,
@@ -22,31 +19,42 @@ export default function App() {
     }));
   }
 
-
   function rollDice() {
-      setDice(prev => prev.map(diceObj => diceObj.isHeld ? diceObj : {...diceObj, value: Math.ceil(Math.random() * 6)} ))
+    gameWon
+      ? setDice(generateAllNewDice())
+      : setDice((prev) =>
+          prev.map((diceObj) =>
+            diceObj.isHeld
+              ? diceObj
+              : { ...diceObj, value: Math.ceil(Math.random() * 6) }
+          )
+        );
   }
 
   function hold(id) {
-    setDice(prev => prev.map(diceObj => diceObj.id === id ? {...diceObj, isHeld: !diceObj.isHeld} : diceObj))
+    setDice((prev) =>
+      prev.map((diceObj) =>
+        diceObj.id === id ? { ...diceObj, isHeld: !diceObj.isHeld } : diceObj
+      )
+    );
   }
 
   const diceElements = dice.map((num) => (
-    <Die 
-      key={num.id} 
-      diceObj={num} 
-      onClick={hold} 
-    />
+    <Die key={num.id} diceObj={num} onClick={hold} />
   ));
   // console.log("Run");
   return (
-        <main>
-            <h1 className="title">Tenzies</h1>
-            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-            <div className="dice-container">
-                {diceElements}
-            </div>
-            <button className="roll-dice" onClick={rollDice}>{gameWon? "New Game" : "Roll"}</button>
-        </main>
-    )
+    <main>
+      {gameWon && <Confetti/>}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        {gameWon ? "New Game" : "Roll"}
+      </button>
+    </main>
+  );
 }
--- Documents/ScrimbaCapstoneProject/Tenzies/src/Die.jsx
+++ Documents/ScrimbaCapstoneProject/Tenzies/src/Die.jsx
@@ -1,6 +1,6 @@
 export default function Die(props) {
     const isHeld = props.diceObj.isHeld ? "#59E319" : ""
     return (
-        <button onClick={() => props.onClick(props.diceObj.id)} style={{backgroundColor: isHeld}}>{props.diceObj.value}</button>
+        <button aria-pressed={props.diceObj.isHeld} aria-label={`Die with value ${props.diceObj.value}, ${props.diceObj.isHeld ? "held" : "not held"}`} onClick={() => props.onClick(props.diceObj.id)} style={{backgroundColor: isHeld}}>{props.diceObj.value}</button>
     )
 }
