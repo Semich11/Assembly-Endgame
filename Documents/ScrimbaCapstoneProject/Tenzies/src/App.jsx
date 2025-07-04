e589d58ef5d648c695821d9ff8f8959ec8f0bf43
semich11 <chima11christopher@gmail.com>
2025-07-04 14:23:59 +0100

    Updated rollDice function to roll only dice that isheld property is false.

--- Documents/ScrimbaCapstoneProject/Tenzies/src/App.jsx
+++ Documents/ScrimbaCapstoneProject/Tenzies/src/App.jsx
@@ -8,25 +8,29 @@ export default function App() {
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => ({
       value: Math.ceil(Math.random() * 6),
-      isHeld: true,
+      isHeld: false,
       id: nanoid(),
     }));
   }
 
-  console.log(dice[0].value);
 
   function rollDice() {
-    setDice(generateAllNewDice());
+      setDice(prev => prev.map(diceObj => diceObj.isHeld ? diceObj : {...diceObj, value: Math.ceil(Math.random() * 6)} ))
   }
 
-  function hold(id){
+  function hold(id) {
     console.log(id)
+    setDice(prev => prev.map(diceObj => diceObj.id === id ? {...diceObj, isHeld: !diceObj.isHeld} : diceObj))
   }
 
   const diceElements = dice.map((num) => (
-    <Die key={num.id} diceObj={num} onClick={hold} />
+    <Die 
+      key={num.id} 
+      diceObj={num} 
+      onClick={hold} 
+    />
   ));
-  console.log("Run");
+  // console.log("Run");
   return (
     <main>
       <div className="dice-container">{diceElements}</div>
--- Documents/ScrimbaCapstoneProject/Tenzies/src/Die.jsx
+++ Documents/ScrimbaCapstoneProject/Tenzies/src/Die.jsx
@@ -1,5 +1,5 @@
 export default function Die(props) {
-    const isHeld = props.diceObj.isHeld ? "#59E319" : "red"
+    const isHeld = props.diceObj.isHeld ? "#59E319" : ""
     return (
         <button onClick={() => props.onClick(props.diceObj.id)} style={{backgroundColor: isHeld}}>{props.diceObj.value}</button>
     )
