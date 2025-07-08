eb145ccc9fa7b2a020dbdbc04a64e91ea3c7d7d7
semich11 <chima11christopher@gmail.com>
2025-07-08 18:38:09 +0100

    Able to display if the letter the user click is correct or wrong.

--- Documents/Assembly_Endgame/src/App.jsx
+++ Documents/Assembly_Endgame/src/App.jsx
@@ -1,77 +1,88 @@
-import { useState } from "react"
-import { languages } from "./languages"
-
-
+import { useState } from "react";
+import { languages } from "./languages";
+import clsx from "clsx";
 
 /**
  * Goal: Allow the user to start guessing the letters
- * 
- * Challenge: Create a new array in state to hold user's
- * guessed letters. When the user chooses a letter, add
- * that letter to this state array.
- * 
- * Don't worry about whether it was a right or wrong 
- * guess yet.
+ *
+ * Challenge: Update the keyboard when a letter is right
+ * or wrong.
+ *
+ * Bonus: use the `clsx` package to easily add conditional
+ * classNames to the keys of the keyboard. Check the docs
+ * to learn how to use it 📖
  */
 
-
 export default function AssemblyEndgame() {
-    const [currentWord, setCurrentWord] = useState("react");
-    const [guessedLetters, setGuessedLetters] = useState([]);
-    
-    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  const [currentWord, setCurrentWord] = useState("react");
+  const [guessedLetters, setGuessedLetters] = useState([]);
 
-    const languageElements = languages.map(lang => {
-        const styles = {
-            backgroundColor: lang.backgroundColor,
-            color: lang.color
-        }
-        return (
-            <span
-                className="chip"
-                style={styles}
-                key={lang.name}
-            >
-                {lang.name}
-            </span>
-        )
-    })
-    
-    const letterElements = currentWord.split("").map((letter, index) => (
-        <span key={index}>{letter.toUpperCase()}</span>
-    ))
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-    const addGuessedletter = (letter) => {
-      setGuessedLetters(prevGuessedLetters => prevGuessedLetters.includes(letter) ? prevGuessedLetters : [...prevGuessedLetters, letter])
-    }
-
-    console.log(guessedLetters);
-    
-    const keyboardElements = alphabet.split("").map(letter => (
-        <button onClick={() => addGuessedletter(letter)} key={letter}>{letter.toUpperCase()}</button>
-    ))
-
+  const languageElements = languages.map((lang) => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
     return (
-        <main>
-            <header>
-                <h1>Assembly: Endgame</h1>
-                <p>Guess the word within 8 attempts to keep the
-                programming world safe from Assembly!</p>
-            </header>
-            <section className="game-status">
-                <h2>You win!</h2>
-                <p>Well done! 🎉</p>
-            </section>
-            <section className="language-chips">
-                {languageElements}
-            </section>
-            <section className="word">
-                {letterElements}
-            </section>
-            <section className="keyboard">
-                {keyboardElements}
-            </section>
-            <button className="new-game">New Game</button>
-        </main>
-    )
+      <span className="chip" style={styles} key={lang.name}>
+        {lang.name}
+      </span>
+    );
+  });
+
+  const letterElements = currentWord
+    .split("")
+    .map((letter, index) => <span key={index}>{letter.toUpperCase()}</span>);
+
+  const addGuessedletter = (letter) => {
+    
+     setGuessedLetters((prevGuessedLetters) =>
+      prevGuessedLetters.includes(letter)
+        ? prevGuessedLetters
+        : [...prevGuessedLetters, letter]
+    );
+    console.log("Clicked!!!")
+  };
+
+  console.log(guessedLetters);
+
+  const keyboardElements = alphabet.split("").map((letter) => {
+    const isGuessed = guessedLetters.includes(letter);
+    const isCorrect = isGuessed && currentWord.includes(letter);
+    const isWrong = isGuessed && !currentWord.includes(letter);
+    console.log(`isGuessed is ${isGuessed} `)
+    return (
+      <button
+      onClick={() => addGuessedletter(letter)}
+      className={clsx({
+        isTrue: isCorrect,
+        isFalse: isWrong
+      })}
+        key={letter}
+      >
+        {letter.toUpperCase()}
+      </button>
+    );
+  });
+
+  return (
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+      <section className="language-chips">{languageElements}</section>
+      <section className="word">{letterElements}</section>
+      <section className="keyboard">{keyboardElements}</section>
+      <button className="new-game">New Game</button>
+    </main>
+  );
 }
