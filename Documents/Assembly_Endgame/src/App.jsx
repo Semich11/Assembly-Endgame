f4a5db047f514a3252c4c5100324ec93a11fa8db
semich11 <chima11christopher@gmail.com>
2025-07-09 18:11:13 +0100

    Add lost className to languageElement is wrongGuesssCount is greater than the index of the languageElement

--- Documents/Assembly_Endgame/src/App.jsx
+++ Documents/Assembly_Endgame/src/App.jsx
@@ -1,91 +1,107 @@
-import { useState } from "react";
-import { languages } from "./languages";
-import clsx from "clsx";
+import { useState } from "react"
+import { clsx } from "clsx"
+import { languages } from "./languages"
 
 /**
- * Goal: Allow the user to start guessing the letters
- *
- * Challenge: Update the keyboard when a letter is right
- * or wrong.
- *
- * Bonus: use the `clsx` package to easily add conditional
- * classNames to the keys of the keyboard. Check the docs
- * to learn how to use it 📖
+ * Goal: Add in the incorrect guesses mechanism to the game
+ * 
+ * Challenge: When mapping over the languages, determine how
+ * many of them have been "lost" and add the "lost" class if
+ * so.
+ * 
+ * Hint: use the wrongGuessCount combined with the index of
+ * the item in the array while inside the languages.map code
  */
 
 export default function AssemblyEndgame() {
-  const [currentWord, setCurrentWord] = useState("react");
-  const [guessedLetters, setGuessedLetters] = useState([]);
+    // State values
+    const [currentWord, setCurrentWord] = useState("react")
+    const [guessedLetters, setGuessedLetters] = useState([])
+    
+    // Derived values
+    const wrongGuessCount = 
+        guessedLetters.filter(letter => !currentWord.includes(letter)).length
+    
+    // Static values
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-  const wrongGuessCount = guessedLetters.filter(element => !currentWord.includes(element)).length;
+    function addGuessedLetter(letter) {
+        setGuessedLetters(prevLetters =>
+            prevLetters.includes(letter) ?
+                prevLetters :
+                [...prevLetters, letter]
+        )
+    }
 
-  console.log(wrongGuessCount)
+    const languageElements = languages.map((lang, index) => {
+        const styles = {
+            backgroundColor: lang.backgroundColor,
+            color: lang.color
+        }
+        const isLost = wrongGuessCount > index ? true : false;
 
-  const alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-  const languageElements = languages.map((lang) => {
-    const styles = {
-      backgroundColor: lang.backgroundColor,
-      color: lang.color,
-    };
-    return (
-      <span className="chip" style={styles} key={lang.name}>
-        {lang.name}
-      </span>
-    );
-  });
-
-  const letterElements = currentWord
-    .split("")
-    .map((letter, index) => {
-      return   (<span key={index}>{guessedLetters.includes(letter) ? letter.toUpperCase() : "" }</span>);//
+        const className = clsx({
+            chip: true,
+            lost:  isLost,
+        })
+        return (
+            <span
+                className={className}
+                style={styles}
+                key={lang.name}
+            >
+                {lang.name}
+            </span>
+        )
     })
 
-  const addGuessedletter = (letter) => {
-    
-     setGuessedLetters((prevGuessedLetters) =>
-      prevGuessedLetters.includes(letter)
-        ? prevGuessedLetters
-        : [...prevGuessedLetters, letter]
-    );
-    
-  };
+    const letterElements = currentWord.split("").map((letter, index) => (
+        <span key={index}>
+            {guessedLetters.includes(letter) ? letter.toUpperCase() : ""}
+        </span>
+    ))
 
-  const keyboardElements = alphabet.split("").map((letter) => {
-    const isGuessed = guessedLetters.includes(letter);
-    const isCorrect = isGuessed && currentWord.includes(letter);
-    const isWrong = isGuessed && !currentWord.includes(letter);
+    const keyboardElements = alphabet.split("").map(letter => {
+        const isGuessed = guessedLetters.includes(letter)
+        const isCorrect = isGuessed && currentWord.includes(letter)
+        const isWrong = isGuessed && !currentWord.includes(letter)
+        const className = clsx({
+            correct: isCorrect,
+            wrong: isWrong
+        })
+        
+        return (
+            <button
+                className={className}
+                key={letter}
+                onClick={() => addGuessedLetter(letter)}
+            >
+                {letter.toUpperCase()}
+            </button>
+        )
+    })
+
     return (
-      <button
-      onClick={() => addGuessedletter(letter)}
-      className={clsx({
-        isTrue: isCorrect,
-        isFalse: isWrong
-      })}
-        key={letter}
-      >
-        {letter.toUpperCase()}
-      </button>
-    );
-  });
-
-  return (
-    <main>
-      <header>
-        <h1>Assembly: Endgame</h1>
-        <p>
-          Guess the word within 8 attempts to keep the programming world safe
-          from Assembly!
-        </p>
-      </header>
-      <section className="game-status">
-        <h2>You win!</h2>
-        <p>Well done! 🎉</p>
-      </section>
-      <section className="language-chips">{languageElements}</section>
-      <section className="word">{letterElements}</section>
-      <section className="keyboard">{keyboardElements}</section>
-      <button className="new-game">New Game</button>
-    </main>
-  );
+        <main>
+            <header>
+                <h1>Assembly: Endgame</h1>
+                <p>Guess the word within 8 attempts to keep the
+                programming world safe from Assembly!</p>
+            </header>
+            <section className="game-status">
+                <h2>You win!</h2>
+                <p>Well done! 🎉</p>
+            </section>
+            <section className="language-chips">
+                {languageElements}
+            </section>
+            <section className="word">
+                {letterElements}
+            </section>
+            <section className="keyboard">
+                {keyboardElements}
+            </section>
+            <button className="new-game">New Game</button>
+        </main>
+    )
 }
